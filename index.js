8ff8da2582b9fcf9d082f6012b64d6423c46ff5e
jenniekibiri <josphatje@gmail.com>
2021-07-15 22:08:20 +0300

    connect to db

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const  express = require ('express');
 const  morgan= require('morgan');
+require('dotenv').config();
 const port = 5000
 const app =express();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGOURL,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+     useUnifiedTopology: true 
+}).then(()=>console.log('DB connected'));
 app.use(morgan('dev'))
 app.get('/', (req, res) => {
     res.send('Hello World!')
