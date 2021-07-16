05cc09c050063da7504076a31e95acb1228e17a4
jenniekibiri <josphatje@gmail.com>
2021-07-16 11:40:09 +0300

    create mongo user schema

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,13 @@
+import { User } from '../models/user.js';
 export default class UserController {
 
     static async getUsers(req,res){
-        res.status(200).send({message:'users'})
+        User.find((err, user) => {
+            if (err) {
+              return res.status(400).json({ error: err });
+            }
+            res.json(user );
+          }).select("name email created updated");
     }
     static async getUser(req,res){
         res.status(200).send({message:'user'})
---
+++ models/user.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  address: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  
+  hashPassword: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  created: {
+    type: Date,
+    default: Date.now(),
+  },
+  updated: Date,
+});
+
+export const User =mongoose.model('user', userSchema);
