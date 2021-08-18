16e8e43ef0ecfba9a4d88a10eddb715ae5b9e185
jenniekibiri <josphatje@gmail.com>
2021-08-18 08:47:59 +0300

    create orders routes

--- routes/orders.js
+++ routes/orders.js
@@ -1,78 +1,43 @@
-const { Order } = require('../models/order');
-const { errorHandler } = require('../helpers/dbErrorHandler');
-// sendgrid for email npm i @sendgrid/mail
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey('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');
+const express = require("express");
+const router = express.Router();
 
-exports.orderById = (req, res, next, id) => {
-    Order.findById(id)
-        .populate('products.product', 'name price')
-        .exec((err, order) => {
-            if (err || !order) {
-                return res.status(400).json({
-                    error: errorHandler(err)
-                });
-            }
-            req.order = order;
-            next();
-        });
-};
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
+const { userById, addOrderToUserHistory } = require("../controllers/user");
+const {
+    create,
+    listOrders,
+    getStatusValues,
+    orderById,
+    updateOrderStatus
+} = require("../controllers/order");
+const { decreaseQuantity } = require("../controllers/product");
 
-exports.create = (req, res) => {
-    console.log('CREATE ORDER: ', req.body);
-    req.body.order.user = req.profile;
-    const order = new Order(req.body.order);
-    order.save((error, data) => {
-        if (error) {
-            return res.status(400).json({
-                error: errorHandler(error)
-            });
-        }
-        // send email alert to admin
-        // order.address
-        // order.products.length
-        // order.amount
-        const emailData = {
-            to: 'kaloraat@gmail.com',
-            from: 'noreply@ecommerce.com',
-            subject: `A new order is received`,
-            html: `
-            <p>Customer name:</p>
-            <p>Total products: ${order.products.length}</p>
-            <p>Total cost: ${order.amount}</p>
-            <p>Login to dashboard to the order in detail.</p>
-        `
-        };
-        sgMail.send(emailData);
-        res.json(data);
-    });
-};
+router.post(
+    "/order/create/:userId",
+    requireSignin,
+    isAuth,
+    addOrderToUserHistory,
+    decreaseQuantity,
+    create
+);
 
-exports.listOrders = (req, res) => {
-    Order.find()
-        .populate('user', '_id name address')
-        .sort('-created')
-        .exec((err, orders) => {
-            if (err) {
-                return res.status(400).json({
-                    error: errorHandler(error)
-                });
-            }
-            res.json(orders);
-        });
-};
+router.get("/order/list/:userId", requireSignin, isAuth, isAdmin, listOrders);
+router.get(
+    "/order/status-values/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    getStatusValues
+);
+router.put(
+    "/order/:orderId/status/:userId",
+    requireSignin,
+    isAuth,
+    isAdmin,
+    updateOrderStatus
+);
 
-exports.getStatusValues = (req, res) => {
-    res.json(Order.schema.path('status').enumValues);
-};
+router.param("userId", userById);
+router.param("orderId", orderById);
 
-exports.updateOrderStatus = (req, res) => {
-    Order.update({ _id: req.body.orderId }, { $set: { status: req.body.status } }, (err, order) => {
-        if (err) {
-            return res.status(400).json({
-                error: errorHandler(err)
-            });
-        }
-        res.json(order);
-    });
-};
+module.exports = router;
