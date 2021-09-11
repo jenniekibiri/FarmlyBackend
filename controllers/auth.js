abbc11ee4086ad9dfed23e3ad41d5838d39db46d
jenniekibiri <josphatje@gmail.com>
2021-09-11 12:55:31 +0300

    bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ dotenv.config();
 export const register = async (req, res, next) => {
   const userExists = await User.findOne({ email: req.body.email });
   if (userExists) {
-    return res.status(403).json({
+    return res.json({
       error: "email is already taken Login!",
     });
   }
